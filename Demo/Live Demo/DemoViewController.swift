fdc7781ba6c184a9abcc33a597f5fe560b1c0cc2
Davide Castello <davide@moku.io>
2020-11-10 22:38:12 +0100

    updated for 2020 presentation

--- Demo/Live Demo/DemoViewController.swift
+++ Demo/Live Demo/DemoViewController.swift
@@ -9,26 +9,15 @@
 import UIKit
 import EXTView
 
+// Image: aspectFill
+// Colors: customBlack and red
+
 class DemoViewController: UIViewController {
-  
-  @IBOutlet weak var image: UIImageView!
-  @IBOutlet weak var highlight: UILabel!
-  @IBOutlet weak var descriptionLabel: UILabel!
-  @IBOutlet weak var reservationLabel: UILabel!
-  @IBOutlet weak var reserveButton: EXTButton!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+  
     self.title = "Royal Suite"
-    image.loadImage(url: Constants.sampleImageURL)
-    highlight.setupWithFontSize(Constants.TEXT_SIZE_24, Constants.customBlack)
-    highlight.text = Constants.sampleHighlight
-    descriptionLabel.setupWithFontSize(Constants.TEXT_SIZE_18, Constants.customBlack)
-    descriptionLabel.text = Constants.sampleDescription
-    reservationLabel.setupWithFontSize(Constants.TEXT_SIZE_12, Constants.red)
-    reservationLabel.text = Constants.sampleReservationText
-    reserveButton.setupMainButtons()
   }
   
 }
